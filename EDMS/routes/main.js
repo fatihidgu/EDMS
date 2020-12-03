b51d61d869a92cb00a0c5968b10a3de34d24e2d9
fatihidgu <fatihidgu@gmail.com>
2020-12-03 21:57:10 +0300

    Onchanges

--- EDMS/routes/main.js
+++ EDMS/routes/main.js
@@ -5,6 +5,9 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('site/index')
 })
+router.get('/onchangefiles', (req, res) => {
+    res.render('site/onchange')
+})
 
 
 
