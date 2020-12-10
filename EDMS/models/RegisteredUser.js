9d1b0025cf93f6ca47e6868071c37dd5e16e8678
fatihidgu <fatihidgu@gmail.com>
2020-12-10 11:44:52 +0300

    .

--- EDMS/models/RegisteredUser.js
+++ EDMS/models/RegisteredUser.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const RegisteredUserSchema = new mongoose.Schema({
     name:{type:String, require:true},
     surname:{type:String, require:true},
-    email:{type:String, require:true, unique:true},
+    email:{type:String, require:true, unique:true,},
     password:{type:String, require:true}
 })
 
