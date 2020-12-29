577705659ab57f440fca950a0a0a4bee501ad5b1
Oğuzhan Aydın <oguzhan.aydin@isik.edu.tr>
2020-12-29 22:00:58 +0300

    work unit conflict handled

--- EDMS/models/WorkflowFileType.js
+++ EDMS/models/WorkflowFileType.js
@@ -6,11 +6,7 @@ const WorkflowFileTypeSchema = new Schema({
     workflowFileTypeCode:{type:String,required:true, unique:true},
     workflowFileTypeName:{type: String,required:true, unique:true},
     creationDate:{type: Date, default:Date.now, required:true},
-<<<<<<< HEAD
     deleteDate:{type:Date, default:null}
-=======
-    deleteDate:{type:Date, default:null,}
->>>>>>> b49a054497a39e7c885295610287b39bfcca5fac
 })
 
 module.exports = mongoose.model('WorkflowFileType',WorkflowFileTypeSchema)
